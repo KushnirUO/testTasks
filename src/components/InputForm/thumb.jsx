f0247f8ad3a49a185c31c6e778525361afb5a6e9
”Julia <kushnir1145@gmail.com>
2022-09-04 15:59:24 +0300

    Initialize project using Create React App

---
+++ src/components/InputForm/thumb.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export class Thumb extends React.Component {
+    state = {
+        loading: false,
+        thumb: undefined,
+    };
+
+    componentWillReceiveProps(nextProps) {
+        if (!nextProps.file) {
+            return;
+        }
+
+        this.setState({loading: true}, () => {
+            let reader = new FileReader();
+
+            reader.onloadend = () => {
+                this.setState({loading: false, thumb: reader.result});
+            };
+            if (nextProps.file) {
+                reader.readAsDataURL(nextProps.file);
+            }
+        });
+    }
+
+    render() {
+        const {file} = this.props;
+        const {loading, thumb} = this.state;
+
+        if (!file) {
+            return null;
+        }
+
+        if (loading) {
+            return <p>loading...</p>;
+        }
+
+        return (<img src={thumb}
+                     alt={file.name}
+                     height='100%'/>);
+    }
+}
